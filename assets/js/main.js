6dd730fbe9f5cc73b1f3b36db9a542f4cd3757ff
MarioBlasi <mblasi509@gmail.com>
2023-03-20 10:14:37 +0000

    funzione Math.random()*16 per generare i numeri

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,10 @@ function startGame() {
     thisCell.addEventListener("click", function () {
       thisCell.classList.toggle("bg_blue");
       console.log("Changed the color" + thisCell.textContent);
+      //per generare i 16 numeri casuali utilizziamo Math.random()*16 e
+      // poi con la funzione Math.round arrotondiamo all’intero.*/
+      num = Math.round(Math.random() * 16);
+      console.log("Numero casuale " + num);
     });
   }
 }
@@ -35,17 +39,38 @@ playButton.addEventListener("click", startGame);
 
 /*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: 
 le bombe.
+
 nella stessa cella può essere posizionata al massimo una bomba, 
 perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
 In seguito l'utente clicca su una cella:
 se il numero è presente nella lista dei numeri generati
 abbiamo calpestato una bomba
 la cella si colora di rosso e la partita termina.
+
 Altrimenti
 la cella cliccata si colora di azzurro
 l'utente può continuare a cliccare sulle altre celle.
+
 La partita termina quando il giocatore clicca su una bomba o quando
  raggiunge il numero massimo possibile di numeri consentiti (ovvero 
   quando ha rivelato tutte le celle che non sono bombe).
+
 Al termine della partita il software deve comunicare il punteggio, 
 cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
+
+/*--------STEP 1----------*/
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: 
+le bombe.
+// per generare i 16 numeri casuali utilizziamo Math.random()*16 e
+// poi con la funzione Math.round arrotondiamo all’intero.*/
+
+// function casuale() {
+//   num = Math.round(Math.random() * 16);
+//   console.log("Numero casuale " + num);
+// }
+// creiamo in html un input button per il pulsante a cui andiamo ad
+//associare l'evento onclick che richiama la funzione casuale()
+
+// associamo l input button alla cell e non al button cosi al click della cella
+//si genererá un numero casuale con il colore impostato celeste
